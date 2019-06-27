8e39524118f588e0b7c8a60eea026c9b16e82a7e
Thanh Phu <thanh.phu@themedium.io>
2019-06-27 15:22:35 +0900

    Add error handling

--- counter.php
+++ counter.php
@@ -6,21 +6,24 @@
     }
 
     $fp = fopen($fileName, "r+");
+    if (!fp) {
+        echo('Temporarily unavailable');
+    } else {
+        while(!flock($fp, LOCK_EX)) {  // acquire an exclusive lock
+            // waiting to lock the file
+        }
 
-    while(!flock($fp, LOCK_EX)) {  // acquire an exclusive lock
-        // waiting to lock the file
+        $str = fread($fp, filesize($fileName));
+        $counter = intval(trim($str));
+        $counter++;
+
+        echo($counter);
+
+        ftruncate($fp, 0);      // truncate file
+        fwrite($fp, $counter);  // set your data
+        fflush($fp);            // flush output before releasing the lock
+        flock($fp, LOCK_UN);    // release the lock
+
+        fclose($fp);
     }
-
-    $str = fread($fp, filesize($fileName));
-    $counter = intval(trim($str));
-    $counter++;
-
-    echo($counter);
-
-    ftruncate($fp, 0);      // truncate file
-    fwrite($fp, $counter);  // set your data
-    fflush($fp);            // flush output before releasing the lock
-    flock($fp, LOCK_UN);    // release the lock
-
-    fclose($fp);
 ?>
