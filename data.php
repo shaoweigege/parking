c9ee87541ee30cb37cfbe6602ea99dfaa9239197
Thanh Phu <git@thanhphu.net>
2020-03-16 11:18:41 +0900

    Add title setting, 301 redirect

--- data.php
+++ data.php
@@ -1,18 +1,74 @@
 <?php
   $data = [
-      "afreeca.vn" => "https://faceboo.kr",
-      "barlaw.kr" => "https://hanquoc.kr/category/tin-phap-luat/",
-      "baybong.com" => "https://hanquoc.kr/ve-may-bay/",
-      "hanquoc.wiki" => "https://hanquoc.kr",
-      "hyperledgerkorea.com" => "https://faceboo.kr/groups/hyperledger",
-      "hyperledgervietnam.com" => "https://faceboo.kr/groups/hyperledger",
-      "kakaotalk.vn" => "https://hanquoc.kr/cau-hoi-thuong-gap-ve-kakaotalk/",
-      "kimchang.vn" => "https://hanquoc.kr/category/tin-phap-luat/",
-      "kii.systems" => "https://netviet.kr",
-      "koreamoneytransfer.com" => "https://hanquoc.kr/how-to-register-and-use-wirebarley-to-transfer-money-home/",
-      "kyeongin.info" => "https://netviet.kr",
-      "lawwin.kr" => "https://hanquoc.kr/category/tin-phap-luat/",
-      "lawwinvn.com" => "https://hanquoc.kr/category/tin-phap-luat/", 
-      "yor.kr" => "https://hanquoc.kr/lien-he"
+      "afreeca.vn" => [
+        "url" => "https://faceboo.kr",
+        "noframe" => FALSE,
+        "title" => "A domain of netviet.kr"
+      ],
+      "barlaw.kr" => [
+        "url" => "https://hanquoc.kr/category/tin-phap-luat/",
+        "noframe" => FALSE,
+        "title" => "A domain of netviet.kr"
+      ],
+      "baybong.com" => [
+        "url" => "https://hanquoc.kr/ve-may-bay/",
+        "noframe" => TRUE,
+        "title" => "Tung tăng bay khắp nơi"
+      ],
+      "hanquoc.wiki" => [
+        "url" => "https://hanquoc.kr",
+        "noframe" => FALSE,
+        "title" => "Wiki Hàn Quốc"
+      ],
+      "hyperledgerkorea.com" => [
+        "url" => "https://faceboo.kr/groups/hyperledger",
+        "noframe" => FALSE,
+        "title" => "Hyperledger community Korea"
+      ],
+      "hyperledgervietnam.com" => [
+        "url" => "https://faceboo.kr/groups/hyperledger",
+        "noframe" => FALSE,
+        "title" => "Hyperledger community Vietnam"
+      ],
+      "kakaotalk.vn" => [
+        "url" => "https://hanquoc.kr/cau-hoi-thuong-gap-ve-kakaotalk/",
+        "noframe" => FALSE,
+        "title" => "A domain of netviet.kr"
+      ],
+      "kimchang.vn" => [
+        "url" => "https://hanquoc.kr/category/tin-phap-luat/",
+        "noframe" => FALSE,
+        "title" => "A domain of netviet.kr"
+      ],
+      "kii.systems" => [
+        "url" => "https://netviet.kr",
+        "noframe" => TRUE,
+        "title" => "A domain of netviet.kr"
+      ],
+      "koreamoneytransfer.com" => [
+        "url" => "https://hanquoc.kr/how-to-register-and-use-wirebarley-to-transfer-money-home/",
+        "noframe" => FALSE,
+        "title" => "A domain of netviet.kr"
+      ],
+      "kyeongin.info" => [
+        "url" => "https://netviet.kr",
+        "noframe" => TRUE,
+        "title" => "A domain of netviet.kr"
+      ],
+      "lawwin.kr" => [
+        "url" => "https://hanquoc.kr/category/tin-phap-luat/",
+        "noframe" => FALSE,
+        "title" => "A domain of netviet.kr"
+      ],
+      "lawwinvn.com" => [
+        "url" => "https://hanquoc.kr/category/tin-phap-luat/", 
+        "noframe" => FALSE,
+        "title" => "A domain of netviet.kr"
+      ],
+      "yor.kr" => [
+        "url" => "https://hanquoc.kr/lien-he",
+        "noframe" => FALSE,
+        "title" => "A domain of netviet.kr"
+      ],
   ];
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+    include 'data.php';
+
+    $url = "https://netviet.kr/domain/";
+    $noframe = FALSE;
+    $title = $_SERVER['SERVER_NAME'];
+    foreach ($data as $key => $value) {
+        if (stristr($_SERVER['SERVER_NAME'], $key)) {
+            $url = $value["url"];
+            $noframe = $value["noframe"];
+            $title = $value["title"];
+            break;
+        }
+    }
+
+    if ($noframe === TRUE) {
+        header("HTTP/1.1 301 Moved Permanently");
+        header("Location: $url");
+        exit();
+    }
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
 <head>
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -8,49 +31,57 @@
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <title><?php echo $title ?></title>
+    <style>
+    iframe {
+        position: absolute;
+        top: 64px;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        border: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        z-index: 999;
+    }
+    </style>
 </head>
 
 <body>
-    <nav>
-        <div class="nav-wrapper">
-            <a id="domainname" href="#" class="brand-logo" style="padding-left: 10px">Domain name</a>
-            <ul id="nav-mobile" class="right" style="padding-right: 10px">
-                <li>
-                    <a id="sendmail">
-                        <i class="material-icons left">mail</i>Let's work together!</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    <iframe src="<?php 
-
-    include 'data.php';
-
-    $found = FALSE;
-    foreach ($data as $key => $value) {
-        if (stristr($_SERVER['SERVER_NAME'], $key)) {
-            echo($value);
-            $found = TRUE;
-            break;
-        }
+    <div class="wrapper">
+    <?php 
+    if ($noframe === TRUE) {
+        ?>
+        <iframe src="<?php echo $url ?>" allowfullscreen="true">
+            Your browser doesn't support iframes
+        </iframe>
+        <?php
+    } else {
+        ?>
+        <nav>
+            <div class="nav-wrapper">
+                <a id="domainname" href="#" class="brand-logo" style="padding-left: 10px"><?php echo $title ?></a>
+                <ul id="nav-mobile" class="right" style="padding-right: 10px">
+                    <li>
+                        <a id="sendmail">
+                            <i class="material-icons left">mail</i>Let's work together!</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <iframe src="<?php echo $url ?>">
+            Your browser doesn't support iframes
+        </iframe>
+        <script>
+            $('#sendmail').click(function () {
+                window.open('mailto:domains@netviet.kr?subject=Domain offer for ' + window.location.hostname);
+            });
+        </script>
+        <?php
     }
-
-    if ($found === FALSE) {
-        echo("https://netviet.kr/domain/");
-    }
-
-    ?>"
-        style="position:fixed; top:64px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
-        Your browser doesn't support iframes
-    </iframe>
-    <script>
-        $(document).ready(function () {
-            $('#domainname').text(window.location.hostname);
-        });
-        $('#sendmail').click(function () {
-            window.open('mailto:domains@netviet.kr?subject=Domain offer for ' + window.location.hostname);
-        });
-    </script>
+    ?>
+    </div>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108055015-8"></script>
     <script>
@@ -61,3 +92,4 @@
         gtag('config', 'UA-108055015-8');
     </script>
 </body>
+</html>
